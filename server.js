c1d524ae537cb6c1d0b70368eec3f537c09bc35b
Rishi Sharma <rsbeoriginal@users.noreply.github.com>
2016-11-03 14:41:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -197,14 +197,14 @@ app.post('/comment',function(req,res){
     // var u_id=req.body.userId;
     // var c=req.body.comment;
     // var f_name=req.body.fullName;
-    
-    pool.query('INSERT INTO comment (article_id,user_id,comment,full_name) VALUES (1,1,\'direct\',\'Rishi\');',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            res.send('Success');
-        }
-    });
+    console.log(req.body);
+    // pool.query('INSERT INTO comment (article_id,user_id,comment,full_name) VALUES (1,1,\'direct\',\'Rishi\');',function(err,result){
+    //     if(err){
+    //         res.status(500).send(err.toString());
+    //     }else{
+    //         res.send('Success');
+    //     }
+    // });
 });
 
 
