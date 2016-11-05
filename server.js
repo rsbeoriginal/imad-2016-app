4974fd7a7e48513973e033082a9a3dd6cc163ca8
Rishi Sharma <rsbeoriginal@users.noreply.github.com>
2016-11-05 11:18:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -272,7 +272,8 @@ app.get('/check-login', function (req, res) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              res.send(result.rows[0].username);    
+             // res.send(result.rows[0].username);    
+             res.send("Hey "+req.session.auth.userFullName);
            }
        });
    } else {
