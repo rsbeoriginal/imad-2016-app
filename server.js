ea530ba3eb6580a713d0fc2d259b9af6563f5ca5
Rishi Sharma <rsbeoriginal@users.noreply.github.com>
2016-11-06 14:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,8 +147,8 @@ function createTemplate (data,comment) {
             </div>
             
         </div>
-        
-      
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
   </body>
 </html>
     `;
